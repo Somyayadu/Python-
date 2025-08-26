7d4e60c1d18926f4f45a5704fd648e621c780cca
Somyayadu <honeyyaduvanshi496@gmail.com>
2025-08-27 03:06:52 +0530

    new file

---
+++ Miniproject-1.py
@@ -0,0 +1,74 @@
+# Mini Project 1
+
+# Ai Desies Pridiction 
+
+print ('🤖 AI Disease Prediction System 🏥✨')
+print (' 🏥 WELCOME 🏥')
+print ('✨ Please match your symptoms with one of the options below 🏥🔍 ')
+print ('A. Excessive thirst💧 ,Frequent urination🚽 ,Unexplained weight loss⚖️')
+print ('B. Persistent cough🤧 ,Night sweats😓 ,Weight loss⚖️')
+print ('C. Severe one-sided headache🤕 ,Nausea/vomiting🤮 ,Sensitivity to light/sound💡🔊')
+print ('D. High fever🤒 ,Cough with phlegm😷 ,Chest pain while breathing💔')
+print ('E. Wheezing sound🌀 ,Shortness of breath🫁 ,Cough at night/early morning🌙')
+print ('F. Headache (especially morning)🤯 ,Dizziness😵 ,Blurred vision👀')
+print ('G. Prolonged fever🤒 ,Abdominal pain🤢 ,Rose spots (rash)🌸')
+print ('H. High fever with chills❄️🤒 ,Sweating😓 ,Headache🤕')
+print ('I. Fever🤒 ,Dry cough😷 ,Loss of taste/smell👅')
+print ('J. High fever🌡️ ,Severe body/joint pain ("breakbone fever")💀 ,Skin rash🤒')
+print ('K. Fatigue/weakness😴 ,Pale skin🧑🏻 ,Shortness of breath😮‍💨')
+print ('L. Persistent sadness😢 ,Loss of interest💔 ,Sleep/appetite changes🛌🍽️')
+print ('M. Abdominal pain (lower right)🤕 ,Nausea/vomiting🤮 ,Fever🤒')
+print ('N. Tremors🤲 ,Stiff muscles💪 ,Slow movement🐢')
+print ('O. Severe watery diarrhea💧 ,Dehydration🥤 ,Muscle cramps💢')
+
+print('\n')
+Symptoms = input('🏥 select a option according your symptoms 🏥 :- ')
+
+if Symptoms == 'A' or  Symptoms == 'a':
+    print('Diabetes Mellitus 🍬💉 ')
+
+elif Symptoms == 'B' or Symptoms == 'b':
+    print('Tuberculosis (TB) 🫁 ')
+
+elif Symptoms == 'C' or Symptoms == 'c':
+    print('Migraine 🤯 ')
+
+elif Symptoms == 'D' or Symptoms == 'd':
+    print('Pneumonia 🌬️🫁 ')
+
+elif Symptoms == 'E' or Symptoms == 'e':
+    print('Asthma 😮‍💨 ')
+
+elif Symptoms == 'F' or Symptoms == 'f':
+    print('Hypertension (High BP) ❤️‍🔥 ')
+
+elif Symptoms == 'G' or Symptoms == 'g':
+    print('Typhoid 🥵 ')
+
+elif Symptoms == 'H' or Symptoms == 'h':
+    print('Malaria 🦟 ')
+
+elif Symptoms == 'I' or Symptoms == 'i':
+    print('COVID-19 🦠 ')
+
+elif Symptoms == 'J' or Symptoms == 'j':
+    print('Dengue 🩸 ')
+
+elif Symptoms == 'K' or Symptoms == 'k':
+    print('Anemia 🧬 ')
+
+elif Symptoms == 'L' or Symptoms == 'l':
+    print('Depression 😞 ')
+
+elif Symptoms == 'M' or Symptoms == 'm':
+    print('Appendicitis ⚠️ ')
+
+elif Symptoms == 'N' or Symptoms == 'n':
+    print('Parkinson’s Disease 🖐️ ')
+
+elif Symptoms == 'O' or Symptoms == 'o':
+    print('Cholera 🚱 ')
+
+else:
+    print('🙏 Kindly visit your nearest hospital 🏥 for a checkup 🩺')
+    print('THANK YOU 🙏')
